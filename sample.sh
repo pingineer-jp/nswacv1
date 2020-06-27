a10a3ed356ef337a68298a150cfddf04c6047810
pingineer-jp <pingineer@gmail.com>
2020-06-28 02:33:54 +0900

    sleepの引数が間違えていたので修正

--- sample.sh
+++ sample.sh
@@ -2,11 +2,11 @@
 
 # サイバー攻撃警報一覧の取得
 python cyberattackalarms.py -v -d 2020-06-23 -t 09:00:00 -r 180
-sleep(1)
+sleep 1
 
 # マルウェア警報一覧の取得
 python malwarealarms.py -v -d 2020-06-23 -t 09:00:00 -r 180
-sleep(1)
+sleep 1
 
 # 標的型サイバー攻撃警報一覧の取得
 python targetedattackalarms.py -v -d 2020-06-23 -t 09:00:00 -r 180
